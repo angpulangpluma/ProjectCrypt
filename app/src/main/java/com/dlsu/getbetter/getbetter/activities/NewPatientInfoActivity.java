4ce1c3ab3c68b13d1a6ce9b60ab1629599c949cc
Marienne Lopez <marienne_lopez@dlsu.edu.ph>
2017-10-27 08:48:36 +0800

    [inclusion again...] serializing pt3

--- app/src/main/java/com/dlsu/getbetter/getbetter/activities/NewPatientInfoActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/activities/NewPatientInfoActivity.java
@@ -1,8 +1,11 @@
 package com.dlsu.getbetter.getbetter.activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -10,6 +13,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,12 +25,22 @@ import android.widget.Spinner;
 
 import com.dlsu.getbetter.getbetter.DirectoryConstants;
 import com.dlsu.getbetter.getbetter.R;
+import com.dlsu.getbetter.getbetter.cryptoGB.Serializator;
+import com.dlsu.getbetter.getbetter.cryptoGB.aes;
 import com.dlsu.getbetter.getbetter.database.DataAdapter;
 import com.dlsu.getbetter.getbetter.sessionmanagers.NewPatientSessionManager;
 import com.dlsu.getbetter.getbetter.sessionmanagers.SystemSessionManager;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -422,5 +436,117 @@ public class NewPatientInfoActivity extends AppCompatActivity implements DatePic
 
     private String getTimeStamp() {
         return new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.getDefault()).format(new Date());
+    }
+
+    private aes cryptoInit(){
+//        Serializator str = new Serializator();
+        checkPermissions(this);
+        File set = null;
+        aes mstr = null;
+        set = createFile(this, "datadb.dat");
+        if(set!=null){
+            mstr = Serializator.deserialize(set.getPath(), aes.class);
+            Log.w("crypto", Boolean.toString(mstr!=null));
+            Log.w("key", String.valueOf(mstr.getKey().getEncoded()));
+        }
+        return mstr;
+    }
+
+    private File createFile(Context con, String newname){
+        checkPermissions(this);
+        File f = null;
+        InputStream in;
+        OutputStream out;
+        boolean isFileUnlocked = false;
+        try {
+            f = con.getFileStreamPath(newname);
+            if (!f.exists()) {
+                if (f.createNewFile()) {
+                    Log.w("file?", "new");
+                    in = new FileInputStream(f);
+                    out = new FileOutputStream(f);
+                    if (IOUtils.copy(in, out)>0) {
+                        Log.w("copy?", "yes");
+                        out.close();
+                        in.close();
+                        if (f.canRead()) {
+                            Log.w("read?", "yes");
+                            try {
+                                long lastmod = f.lastModified();
+                                Log.w("last modified", Long.toString(lastmod));
+                                org.apache.commons.io.FileUtils.touch(f);
+                                isFileUnlocked = true;
+                            } catch (IOException e) {
+                                //                            isFileUnlocked = false;
+                                Log.w("error", e.getMessage());
+                            }
+                        } else Log.w("read?", "no");
+                    } else Log.w("copy?", "no");
+                } else Log.w("file?", "no");
+            } else Log.w("exists?", "yes");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    private File createFileDuplicate(String path, String newname, String oldfile){
+        checkPermissions(this);
+        File f = null;
+        InputStream in;
+        OutputStream out;
+        boolean isFileUnlocked = false;
+        try {
+            f = new File(path, newname + "." +
+                    FilenameUtils.getExtension(oldfile));
+            if(f.createNewFile()) {
+                Log.w("file?", "yes");
+                in = new FileInputStream(new File(oldfile));
+                out = new FileOutputStream(f);
+                if (IOUtils.copy(in, out)>-1) {
+                    Log.w("copy?", "yes");
+                    out.close();
+                    in.close();
+                    if (f.canRead()) {
+                        Log.w("exists?", "yes");
+                        try {
+                            long lastmod = f.lastModified();
+                            Log.w("last modified", Long.toString(lastmod));
+                            org.apache.commons.io.FileUtils.touch(f);
+                            isFileUnlocked = true;
+                        } catch (IOException e) {
+                            //                            isFileUnlocked = false;
+                            Log.w("error", e.getMessage());
+                        }
+                    } else Log.w("exists?", "no");
+                } else Log.w("copy?", "no");
+            } else {
+                f = null;
+                Log.w("file?", "no");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    private void checkPermissions(Context context){
+        int readStuff = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        int writeStuff = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        Log.w("read?", Integer.toString(readStuff));
+//        Log.w("write?", Integer.toString(writeStuff));
+        //for read stuff
+        if(readStuff == PackageManager.PERMISSION_GRANTED)
+            Log.w("read?", "yes");
+        else if(readStuff == PackageManager.PERMISSION_DENIED)
+            Log.w("read?", "no");
+
+        //for write stuff
+        if(writeStuff == PackageManager.PERMISSION_GRANTED)
+            Log.w("write?", "yes");
+        else if(writeStuff == PackageManager.PERMISSION_DENIED)
+            Log.w("write?", "no");
     }
 }
