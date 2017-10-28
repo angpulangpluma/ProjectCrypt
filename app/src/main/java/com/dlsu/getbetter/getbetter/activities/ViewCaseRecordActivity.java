195864fed6ebf3730bfbf3469bdb31765677d0a1
Marienne Lopez <marienne_lopez@dlsu.edu.ph>
2017-10-29 00:03:55 +0800

    [inclusion again...] encrypting files FOR REAL pt5

--- app/src/main/java/com/dlsu/getbetter/getbetter/activities/ViewCaseRecordActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/activities/ViewCaseRecordActivity.java
@@ -1,11 +1,15 @@
 package com.dlsu.getbetter.getbetter.activities;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -19,6 +23,8 @@ import android.widget.TextView;
 
 import com.dlsu.getbetter.getbetter.R;
 import com.dlsu.getbetter.getbetter.adapters.FileAttachmentsAdapter;
+import com.dlsu.getbetter.getbetter.cryptoGB.aes;
+import com.dlsu.getbetter.getbetter.cryptoGB.file_aes;
 import com.dlsu.getbetter.getbetter.database.DataAdapter;
 import com.dlsu.getbetter.getbetter.objects.Attachment;
 import com.dlsu.getbetter.getbetter.objects.CaseRecord;
@@ -26,7 +32,15 @@ import com.dlsu.getbetter.getbetter.objects.DividerItemDecoration;
 import com.dlsu.getbetter.getbetter.objects.Patient;
 import com.dlsu.getbetter.getbetter.sessionmanagers.SystemSessionManager;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -95,6 +109,7 @@ public class ViewCaseRecordActivity extends AppCompatActivity implements MediaCo
         Log.d(TAG, "onCreate: " + recordedHpiOutputFile);
         nMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
+        doSomethingCryptFile("dec", new File(recordedHpiOutputFile));
         try {
             nMediaPlayer.setDataSource(recordedHpiOutputFile);
             nMediaPlayer.prepare();
@@ -137,6 +152,7 @@ public class ViewCaseRecordActivity extends AppCompatActivity implements MediaCo
         String fullName = patientInfo.getFirstName() + " " + patientInfo.getMiddleName() + " " + patientInfo.getLastName();
         String gender = patientInfo.getGender();
         String patientAgeGender = patientInfo.getAge() + " yrs. old, " + gender;
+        doSomethingCryptFile("dec", new File(patientInfo.getProfileImageBytes()));
         setPic(profilePic, patientInfo.getProfileImageBytes());
 
         ageGender.setText(patientAgeGender);
@@ -170,6 +186,7 @@ public class ViewCaseRecordActivity extends AppCompatActivity implements MediaCo
 
                 if(caseAttachments.get(position).getAttachmentType() == 1) {
                     Intent intent = new Intent(ViewCaseRecordActivity.this, ViewImageActivity.class);
+                    doSomethingCryptFile("dec", new File(caseAttachments.get(position).getAttachmentPath()));
                     intent.putExtra("imageUrl", caseAttachments.get(position).getAttachmentPath());
                     intent.putExtra("imageTitle", caseAttachments.get(position).getAttachmentDescription());
                     startActivity(intent);
@@ -256,7 +273,10 @@ public class ViewCaseRecordActivity extends AppCompatActivity implements MediaCo
         int id = v.getId();
 
         if(id == R.id.view_case_back_btn) {
-
+            for (int i=0; i<caseAttachments.size(); i++){
+                doSomethingCryptFile("enc", new File(caseAttachments.get(i).getAttachmentPath()));
+            }
+            doSomethingCryptFile("enc", new File(patientInfo.getProfileImageBytes()));
             finish();
 
         } else if (id == R.id.update_case_record_btn) {
@@ -376,6 +396,178 @@ public class ViewCaseRecordActivity extends AppCompatActivity implements MediaCo
     @Override
     public void start() {
         nMediaPlayer.start();
+    }
+
+    private void doSomethingCryptFile(String dec, File input){
+
+        Log.w("service in", "yes");
+
+        file_aes mastercry = new file_aes(cryptoInit(new File("crypto.dat")));
+//        File path = new File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS),
+//                DirectoryConstants.CRYPTO_FOLDER);
+//        path.mkdirs();
+//        File output = new File(path.getPath() +"/" + input.getName());
+//        File output = new File(path.getPath() +"/" + input.getName());
+//        Log.w("output", output.getAbsolutePath());
+        try {
+            FileOutputStream fos = new FileOutputStream(input);
+            fos.write(read(input));
+            fos.flush();
+            fos.close();
+        } catch(Exception e){
+            Log.w("error", e.toString());
+        }
+        switch(dec){
+            case "enc":{
+                mastercry.encryptFile(input);
+                Log.d("Action", "enc");
+            }; break;
+            case "dec":{
+                mastercry.decryptFile(input);
+                Log.d("Action", "dec");
+            }; break;
+        }
+//
+    }
+
+    private byte[] read(File file) throws IOException{
+        byte[] buffer = new byte[(int) file.length()];
+        InputStream ios = null;
+        try{
+            ios = new FileInputStream(file);
+            if(ios.read(buffer)==-1){
+                throw new IOException(
+                        "EOF reached while trying to read the whole file.");
+            }
+        } finally{
+            try {
+                if (ios != null) ios.close();
+            } catch (IOException e){
+                Log.w("error", e.getMessage());
+            }
+        }
+        return buffer;
+    }
+
+    private aes cryptoInit(File set) {
+        checkPermissions(this);
+//        File set = null;
+//        OutputStream in = null;
+//        DataOutputStream dos = null;
+        set = createFile(this, "crypto.dat");
+        aes master = null;
+        if(set!=null){
+            try{
+                master = new aes();
+                master.loadKey(set);
+                master.setCipher();
+//                master.saveKey(master.getKey(), set);
+//                in = new FileOutputStream(set);
+//                dos = new DataOutputStream(in);
+//                dos.write(master.getKey().getEncoded());
+            } catch(Exception e){
+                Log.w("error", e.getMessage());
+            }
+        }
+        return master;
+    }
+
+    private File createFile(Context con, String newname){
+        checkPermissions(this);
+        File f = null;
+        InputStream in;
+        OutputStream out;
+        boolean isFileUnlocked = false;
+        try {
+            f = con.getFileStreamPath(newname);
+            if (!f.exists()) {
+                if (f.createNewFile()) {
+                    Log.w("file?", "new");
+                    in = new FileInputStream(f);
+                    out = new FileOutputStream(f);
+                    if (IOUtils.copy(in, out)>0) {
+                        Log.w("copy?", "yes");
+                        out.close();
+                        in.close();
+                        if (f.canRead()) {
+                            Log.w("read?", "yes");
+                            try {
+                                long lastmod = f.lastModified();
+                                Log.w("last modified", Long.toString(lastmod));
+                                org.apache.commons.io.FileUtils.touch(f);
+                                isFileUnlocked = true;
+                            } catch (IOException e) {
+                                //                            isFileUnlocked = false;
+                                Log.w("error", e.getMessage());
+                            }
+                        } else Log.w("read?", "no");
+                    } else Log.w("copy?", "no");
+                } else Log.w("file?", "no");
+            } else Log.w("exists?", "yes");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    private File createFileDuplicate(String path, String newname, String oldfile){
+        checkPermissions(this);
+        File f = null;
+        InputStream in;
+        OutputStream out;
+        boolean isFileUnlocked = false;
+        try {
+            f = new File(path, newname + "." +
+                    FilenameUtils.getExtension(oldfile));
+            if(f.createNewFile()) {
+                Log.w("file?", "yes");
+                in = new FileInputStream(new File(oldfile));
+                out = new FileOutputStream(f);
+                if (IOUtils.copy(in, out)>-1) {
+                    Log.w("copy?", "yes");
+                    out.close();
+                    in.close();
+                    if (f.canRead()) {
+                        Log.w("exists?", "yes");
+                        try {
+                            long lastmod = f.lastModified();
+                            Log.w("last modified", Long.toString(lastmod));
+                            org.apache.commons.io.FileUtils.touch(f);
+                            isFileUnlocked = true;
+                        } catch (IOException e) {
+                            //                            isFileUnlocked = false;
+                            Log.w("error", e.getMessage());
+                        }
+                    } else Log.w("exists?", "no");
+                } else Log.w("copy?", "no");
+            } else {
+                f = null;
+                Log.w("file?", "no");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    private void checkPermissions(Context context){
+        int readStuff = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        int writeStuff = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        Log.w("read?", Integer.toString(readStuff));
+//        Log.w("write?", Integer.toString(writeStuff));
+        //for read stuff
+        if(readStuff == PackageManager.PERMISSION_GRANTED)
+            Log.w("read?", "yes");
+        else if(readStuff == PackageManager.PERMISSION_DENIED)
+            Log.w("read?", "no");
+
+        //for write stuff
+        if(writeStuff == PackageManager.PERMISSION_GRANTED)
+            Log.w("write?", "yes");
+        else if(writeStuff == PackageManager.PERMISSION_DENIED)
+            Log.w("write?", "no");
     }
 
 }
