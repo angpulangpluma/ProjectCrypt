935cdd1ccd33a2a92c9a4c4299498ac02c1eddaf
Marienne Lopez <marienne_lopez@dlsu.edu.ph>
2017-10-28 22:59:34 +0800

    [inclusion again...] inclusion pt3

--- app/src/main/java/com/dlsu/getbetter/getbetter/activities/RecordHpiActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/activities/RecordHpiActivity.java
@@ -106,7 +106,7 @@ public class RecordHpiActivity extends AppCompatActivity implements View.OnClick
         } else {
             initializeMediaRecorder();
         }
-        cryptoInit();
+        cryptoInit(new File("crypto.dat"));
     }
 
     private void bindViews (RecordHpiActivity activity) {
@@ -313,6 +313,38 @@ public class RecordHpiActivity extends AppCompatActivity implements View.OnClick
         return recordHpiActivity;
     }
 
+    private void doSomethingCryptFile(String dec, File input){
+
+        Log.w("service in", "yes");
+
+        file_aes mastercry = new file_aes(cryptoInit(new File("crypto.dat")));
+        File path = new File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS),
+                DirectoryConstants.CRYPTO_FOLDER);
+//        path.mkdirs();
+//        File output = new File(path.getPath() +"/" + input.getName());
+        File output = new File(path.getPath() +"/" + input.getName());
+        Log.w("output", output.getAbsolutePath());
+        try {
+            FileOutputStream fos = new FileOutputStream(output);
+            fos.write(read(input));
+            fos.flush();
+            fos.close();
+        } catch(Exception e){
+            Log.w("error", e.toString());
+        }
+        switch(dec){
+            case "enc":{
+                mastercry.encryptFile(output);
+                Log.d("Action", "enc");
+            }; break;
+            case "dec":{
+                mastercry.decryptFile(input);
+                Log.d("Action", "dec");
+            }; break;
+        }
+//
+    }
+
     private byte[] read(File file) throws IOException{
         byte[] buffer = new byte[(int) file.length()];
         InputStream ios = null;
@@ -326,56 +358,49 @@ public class RecordHpiActivity extends AppCompatActivity implements View.OnClick
             try {
                 if (ios != null) ios.close();
             } catch (IOException e){
-
+                Log.w("error", e.getMessage());
             }
         }
         return buffer;
     }
 
-    private void doSomethingCryptFile(String dec, File input){
-
-        Log.d("service in", "yes");
-
-        file_aes mastercry = new file_aes(cryptoInit());
-        File path = new File(Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS),
-                DirectoryConstants.CRYPTO_FOLDER);
-        path.mkdirs();
-        File output = new File(path.getPath() +"/" + input.getName());
-        Log.d("output", output.getAbsolutePath());
-        try {
-            FileOutputStream fos = new FileOutputStream(output);
-            fos.write(read(input));
-            fos.flush();
-            fos.close();
-        } catch(Exception e){
-            Log.e("error", e.toString());
-        }
-        switch(dec){
-            case "enc":{
-                mastercry.encryptFile(output);
-                Log.d("Action", "enc");
-            }; break;
-            case "dec":{
-                mastercry.decryptFile(input);
-                Log.d("Action", "dec");
-            }; break;
-        }
-    }
-
-    private aes cryptoInit(){
-//        Serializator str = new Serializator();
+    private aes cryptoInit(File set) {
         checkPermissions(this);
-        File set = null;
-        aes mstr = null;
-        set = createFile(this, "datadb.dat");
+//        File set = null;
+//        OutputStream in = null;
+//        DataOutputStream dos = null;
+        set = createFile(this, "crypto.dat");
+        aes master = null;
         if(set!=null){
-            mstr = Serializator.deserialize(set.getPath(), aes.class);
-            Log.w("crypto", Boolean.toString(mstr!=null));
-            Log.w("key", String.valueOf(mstr.getKey().getEncoded()));
-            Log.w("cipher", Boolean.toString(mstr.getCipher()!=null));
+            try{
+                master = new aes();
+                master.loadKey(set);
+                master.setCipher();
+//                master.saveKey(master.getKey(), set);
+//                in = new FileOutputStream(set);
+//                dos = new DataOutputStream(in);
+//                dos.write(master.getKey().getEncoded());
+            } catch(Exception e){
+                Log.w("error", e.getMessage());
+            }
         }
-        return mstr;
+        return master;
     }
+
+//    private aes cryptoInit(){
+////        Serializator str = new Serializator();
+//        checkPermissions(this);
+//        File set = null;
+//        aes mstr = null;
+//        set = createFile(this, "datadb.dat");
+//        if(set!=null){
+//            mstr = Serializator.deserialize(set.getPath(), aes.class);
+//            Log.w("crypto", Boolean.toString(mstr!=null));
+//            Log.w("key", String.valueOf(mstr.getKey().getEncoded()));
+//            Log.w("cipher", Boolean.toString(mstr.getCipher()!=null));
+//        }
+//        return mstr;
+//    }
 
     private File createFile(Context con, String newname){
         checkPermissions(this);
